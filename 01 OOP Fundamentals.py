3fac8fdecab9e8e6d29bf762c964764d057d897b
janmlew <janmikolajlewinski@gmail.com>
2023-11-18 21:03:36 +0100

    Added code.

--- 01 OOP Fundamentals.py
+++ 01 OOP Fundamentals.py
@@ -10,3 +10,19 @@ mc = MyCounter()
 mc.set_count(5)
 mc.count = mc.count + 1
 print(mc.count)
+
+
+class Employee:
+    def __init__(self):
+        self.name = None
+
+    def set_name(self, new_name: str):
+        assert isinstance(new_name, str)
+        self.name = new_name
+
+
+emp = Employee()
+
+emp.set_name('Korel Rossi')
+
+print(emp.name)
