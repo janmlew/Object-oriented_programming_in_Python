3141f6fb2037df5b4483497249cf06173f038411
janmlew <janmikolajlewinski@gmail.com>
2023-11-19 22:18:36 +0100

    Added code.

--- 02 Inheritance and Polymorphism.py
+++ 02 Inheritance and Polymorphism.py
@@ -1,5 +1,6 @@
 class Player:
     MAX_POSITION = 10
+    MAX_SPEED = 3
 
     def __init__(self):
         self.position = 0
@@ -19,3 +20,37 @@ p = Player(); p.draw()
 p.move(4); p.draw()
 p.move(5); p.draw()
 p.move(3); p.draw()
+
+p1, p2 = Player(), Player()
+print("MAX_SPEED of p1 and p2 before assignment:")
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+p1.MAX_SPEED = 7
+
+print("MAX_SPEED of p1 and p2 after assignment:")
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+print("MAX_SPEED of Player:")
+print(Player.MAX_SPEED)
+
+# Create Players p1 and p2
+p1, p2 = Player(), Player()
+
+print("MAX_SPEED of p1 and p2 before assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+# ---MODIFY THIS LINE---
+Player.MAX_SPEED = 7
+
+print("MAX_SPEED of p1 and p2 after assignment:")
+# Print p1.MAX_SPEED and p2.MAX_SPEED
+print(p1.MAX_SPEED)
+print(p2.MAX_SPEED)
+
+print("MAX_SPEED of Player:")
+# Print Player.MAX_SPEED
+print(Player.MAX_SPEED)
