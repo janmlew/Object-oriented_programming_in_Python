8b70621f2c3510cb9d2fc9f1a85d5e78892f70b3
janmlew <janmikolajlewinski@gmail.com>
2023-11-21 08:39:52 +0100

    Added code delving into comparisons of class instances.

--- 03 Integrating with standard Python.py
+++ 03 Integrating with standard Python.py
@@ -7,7 +7,13 @@ class BankAccount:
         self.balance -= amount
 
     def __eq__(self, other):
-        return self.number == other.number
+        # return (self.number == other.number) and (type(self) == type(other))
+        return (self.number == other.number) and (isinstance(self, BankAccount) == isinstance(other, BankAccount))
+
+
+class Phone:
+    def __init__(self, number=0):
+        self.number = number
 
 
 acct1 = BankAccount(123, 1000)
@@ -15,3 +21,7 @@ acct2 = BankAccount(123, 1000)
 acct3 = BankAccount(456, 1000)
 print(acct1 == acct2)
 print(acct1 == acct3)
+
+acct = BankAccount(873555333)
+pn = Phone(873555333)
+print(acct == pn)
