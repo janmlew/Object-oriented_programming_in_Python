6efcfae3248ac189e810c6561977c3be2042a55c
janmlew <janmikolajlewinski@gmail.com>
2023-11-21 21:10:09 +0100

    Added code with further tests.

--- 03 Integrating with standard Python.py
+++ 03 Integrating with standard Python.py
@@ -25,3 +25,13 @@ print(acct1 == acct3)
 acct = BankAccount(873555333)
 pn = Phone(873555333)
 print(acct == pn)
+
+my_num = 5
+my_str = "Hello"
+
+f = "my_num is {0}, and my_str is {1}.".format(my_num, my_str)
+print(f)
+f = "my_num is {}, and my_str is \"{}\".".format(my_num, my_str)
+print(f)
+f = "my_num is {n}, and my_str is '{s}'.".format(n=my_num, s=my_str)
+print(f)
