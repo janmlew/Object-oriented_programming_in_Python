8ef9685fb772c4d789c55cc72c06e328d69a4f78
janmlew <janmikolajlewinski@gmail.com>
2023-11-22 08:26:39 +0100

    Added __str__() method.

--- 03 Integrating with standard Python.py
+++ 03 Integrating with standard Python.py
@@ -35,3 +35,24 @@ f = "my_num is {}, and my_str is \"{}\".".format(my_num, my_str)
 print(f)
 f = "my_num is {n}, and my_str is '{s}'.".format(n=my_num, s=my_str)
 print(f)
+
+
+class Employee:
+    salary: int
+    name: str
+
+    def __init__(self, name, salary=30000):
+        self.name, self.salary = name, salary
+
+    # Add the __str__() method
+    def __str__(self):
+        cust_str = """
+        Employee name: {name}
+        Employee salary: {salary}""".format(name=self.name, salary=self.salary)
+        return cust_str
+
+
+emp1 = Employee("Amar Howard", 30000)
+print(emp1)
+emp2 = Employee("Carolyn Ramirez", 35000)
+print(emp2)
