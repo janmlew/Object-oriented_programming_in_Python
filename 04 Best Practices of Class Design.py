e10769855e4c8079d10720f32de707dfe9a16b73
janmlew <janmikolajlewinski@gmail.com>
2023-11-24 10:24:03 +0100

    Added code with private attributes added to a class.

--- 04 Best Practices of Class Design.py
+++ 04 Best Practices of Class Design.py
@@ -30,3 +30,29 @@ sq.h = 7
 rec = Rectangle(3, 5)
 print(sq.h)
 print(sq.w)
+
+
+class BetterDate:
+    _MAX_DAYS = 31
+    _MAX_MONTHS = 12
+
+    def __init__(self, year, month, day):
+        self.year, self.month, self.day = year, month, day
+
+    @classmethod
+    def from_str(cls, datestr):
+        year, month, day = map(int, datestr.split("-"))
+        return cls(year, month, day)
+
+    def _is_valid(self):
+        if (self.day <= self._MAX_DAYS) and (self.month <= self._MAX_MONTHS):
+            return True
+        else:
+            return False
+
+
+bd1 = BetterDate(2020, 4, 30)
+print(bd1._is_valid())
+
+bd2 = BetterDate(2020, 6, 45)
+print(bd2._is_valid())
