492396dc3d7e655c13672cd7e4b0211514dc8ad6
janmlew <janmikolajlewinski@gmail.com>
2023-11-24 11:45:03 +0100

    Added code testing read-only attributes.

--- 04 Best Practices of Class Design.py
+++ 04 Best Practices of Class Design.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from datetime import datetime
+
+
 class Rectangle:
     def __init__(self, w, h):
         self.h = h
@@ -82,4 +86,25 @@ class Customer:
 c = Customer('test', 1000)
 c.balance = 2000
 print(c.balance)
+
+
 # c.balance = -1000
+
+
+class LoggedDF(pd.DataFrame):
+    def __init__(self, *args, **kwargs):
+        pd.DataFrame.__init__(self, *args, **kwargs)
+        self.created_at = datetime.today()
+
+    def to_csv(self, *args, **kwargs):
+        temp = self.copy()
+        temp["created_at"] = self.created_at
+        pd.DataFrame.to_csv(temp, *args, **kwargs)
+
+
+# Instantiate a LoggedDF called ldf
+ldf = LoggedDF({"col1": [1, 2], "col2": [3, 4]})
+
+# Assign a new value to ldf's created_at attribute and print
+ldf.created_at = "2035-07-13"
+print(ldf.created_at)
