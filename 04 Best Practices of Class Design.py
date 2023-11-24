506655c4c282861141f3503cdfcf91eb72a1cf99
janmlew <janmikolajlewinski@gmail.com>
2023-11-24 08:44:52 +0100

    Added code testing issues with improperly constructed inheritance.

--- 04 Best Practices of Class Design.py
+++ 04 Best Practices of Class Design.py
@@ -1,14 +1,25 @@
 class Rectangle:
-    def __init__(self, h, w):
+    def __init__(self, w, h):
         self.h = h
+        self.w = w
+
+    def set_h(self, h):
+        self.h = h
+
+    def set_w(self, w):
         self.w = w
 
 
 class Square(Rectangle):
-    def __init__(self, w):
-        Rectangle.__init__(self, w, w)
+    def __init__(self, w, h):
+        super().__init__(w, h)
         self.w = w
         self.h = w
+
+    def set_h(self, h):
+        self.h = h
+        self.w = h
+
 
 sq = Square(4)
 sq.h = 7
